7ee5037ecfb9ea87d0ef1c0339fe598838e7cdf3
kishanhitk <kishansharma1231@gmail.com>
2021-04-10 12:35:41 +0530

    ✨ Add emoji face detector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😁": "Daant Khishorna",
+  "😀": "Muskan Ki Chamkan",
+  "🙁": "Sad",
+  "😋": "Yummy",
+  "🙃": "Ajeeb Smile",
+  "😂": "Hahahha..",
+  "😚": "Chummi"
+};
+export default function App() {
+  var emojiInDB = Object.keys(emojiDictionary);
+
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "Iska answer to nhi pata mujhe!😅";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome Inshaan</h1>
+      <input onChange={emojiInputHandler} placeholder="Yahan emoji Daalo" style={{ fontSize: "4vh", padding: "3px" }} />
+      <div style={{ fontSize: "8vh", padding: "3px" }}>{meaning}</div>
+      <h2>Isme se ek chuno:</h2>
+      {emojiInDB.map((emoji) => {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "8vh", padding: "3px", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
